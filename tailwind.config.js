0536bed4b01eeb976f6ad1f02251036562f00a00
karolis-KK <karkis315@gmail.com>
2026-09-08 18:16:18 +0300

    pakeiciau teksta kai kur

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,28 @@
-/** @type {import('tailwindcss').Config} */ 
+/** @type {import('tailwindcss').Config} */
+
 module.exports = {
-  content: ["./dist/*.{html,js}"],
+  content: [
+    "./*.html",
+    "./*.js",
+  ],
+
   theme: {
     screens: {
-      'sm': '640px',
-      'md': '768px',
-      'lg': '1467px',
-      'xl': '1500px',
-      '2xl': '1536px',
+      sm: "640px",
+      md: "768px",
+      lg: "1467px",
+      xl: "1500px",
+      "2xl": "1536px",
     },
-    extend: { 
-      fontFamily: { 
+
+    extend: {
+      fontFamily: {
         inter: ["Inter"],
       },
     },
   },
+
   plugins: [
-    require('@tailwindcss/forms'),
+    require("@tailwindcss/forms"),
   ],
 }
